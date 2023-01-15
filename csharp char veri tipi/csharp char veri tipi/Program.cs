4593bea1f554bc7b491d6a8e49216f9a3a3df40b
Okan Çelebi <okancelebi06@gmail.com>
2023-01-15 23:30:28 +0300

    Add files via upload

---
+++ csharp char veri tipi/csharp char veri tipi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_char_veri_tipi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Karakterleri tutmak için Char veri tipi kullanılır.
+            //Tek tırnak içerisinde tek değer olacak şekilde kullanılır.
+            Char karakter = 'E';
+            Char karakter2 = '?';
+            Console.WriteLine(karakter);
+            Console.WriteLine(karakter2);
+            Console.ReadLine();
+        }
+    }
+}
